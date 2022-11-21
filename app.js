0c96f9c8d38cbe7d0efe6f0a12527e2bef64b87d
lakshyanakc <lakshyana@buildchange.org>
2022-11-20 22:45:03 -0500

    added start param as node app.js

--- app.js
+++ app.js
@@ -39,5 +39,6 @@ TuitsController(app);
 
 // app.listen(4000)
 app.listen(process.env.PORT || 4000);
+
 // we can't use port 4000 when running on Heroku.
 // Instead Heroku declares the proper port to use in an environment variable called PORT available from Node using process.env.PORT
