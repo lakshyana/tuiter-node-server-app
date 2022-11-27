1fd1f5a3e4b8272cefd43fecd1c10bf8800efde8
lakshyanakc <lakshyana@buildchange.org>
2022-11-27 12:43:20 -0500

    updated tuits schema with remaining attributes of a tuit

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -7,5 +7,14 @@ const schema = mongoose.Schema({
     tuit: String, // tuit property of type String
     likes: Number, // likes property of type Number
     liked: Boolean, // liked property of type Boolean
+    // ADD OTHER ATTRIBUTES
+    topic: String,
+    userName:String,
+    handle:String,
+    time: String,
+    image: String,
+    title:String,
+    replies:Number,
+    retuits: Number
     }, {collection: 'tuits'});// collection name where tuits are stored in tuiter database
 export default schema; // export schema so it can be used elsewhere
